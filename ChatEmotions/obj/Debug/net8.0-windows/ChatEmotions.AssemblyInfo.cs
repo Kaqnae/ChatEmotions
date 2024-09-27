a8ff9d9aba90301feb6947986f7579b9642e36ba
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2024-09-27 09:56:30 +0200

    updated interface and adapter

--- ChatEmotions/obj/Debug/net8.0-windows/ChatEmotions.AssemblyInfo.cs
+++ ChatEmotions/obj/Debug/net8.0-windows/ChatEmotions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatEmotions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b462cff015442a63b118852079a9df46b4bd241")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10acd3e1eafd0f8e38e39bc110eb4622df6c9ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatEmotions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatEmotions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
